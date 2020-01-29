60e1f8cb470779bafbb1e36b2edab2c803e5d109
Jesse Katsumata <jesse.katsumata@gmail.com>
2020-01-29 19:46:22 +0900

    chore: rename to @react-native-community/clipboard

--- index.d.ts
+++ index.d.ts
@@ -1,6 +1,6 @@
 // CREDITS: This types are based on the original work made by all the people who contributed to @types/react-native
 
-declare module "@react-native-community/react-native-clipboard" {
+declare module "@react-native-community/clipboard" {
   export interface ClipboardStatic {
     /**
      * Get content of string type
