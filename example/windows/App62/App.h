ba5b8c176caede5967e7926307b1e8f7bc1ddaf0
Alexander Sklar <asklar@winse.microsoft.com>
2020-09-01 15:09:19 -0700

    add test app

---
+++ example/windows/App62/App.h
@@ -0,0 +1,13 @@
+#pragma once
+
+#include "App.xaml.g.h"
+
+namespace winrt::App62::implementation
+{
+    struct App : AppT<App>
+    {
+        App() noexcept;
+    };
+} // namespace winrt::App62::implementation
+
+
