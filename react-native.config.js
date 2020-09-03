e44041bd443986e702c305fa5f37089d9127e7b0
Alexander Sklar <asklar@winse.microsoft.com>
2020-09-03 07:21:19 -0700

    cleanup

--- react-native.config.js
+++ react-native.config.js
@@ -4,11 +4,3 @@ module.exports = {
     ios: {project: './example/ios/example.xcworkspace'},
   },
 };
-
-const windowsSwitch = '--use-react-native-windows';
-
-if (process.argv.includes(windowsSwitch)) {
-  process.argv = process.argv.filter(arg => arg !== windowsSwitch);
-  process.argv.push('--config=example/metro.config.windows.js');
-  module.exports.reactNativePath = 'node_modules/react-native-windows';
-}
