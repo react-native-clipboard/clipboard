6286035cf3260eb486d22d93b2748f6ac80e74b3
Alexander Sklar <asklar@microsoft.com>
2020-09-04 03:56:45 -0700

    PR feedback

--- windows/Clipboard/Clipboard.cpp
+++ windows/Clipboard/Clipboard.cpp
@@ -1,3 +1,34 @@
 #include "pch.h"
 #include "Clipboard.h"
-#include <../Common/unicode.cpp>
+#include <Unicode.h>
+
+namespace NativeClipboard {
+  void ClipboardModule::GetString(bool error, React::ReactPromise<React::JSValue>&& result) noexcept {
+    if (error) {
+      result.Reject("Failure");
+    }
+    else {
+      auto dataPackageView = datatransfer::Clipboard::GetContent();
+      if (dataPackageView.Contains(datatransfer::StandardDataFormats::Text())) {
+        dataPackageView.GetTextAsync().Completed([result, dataPackageView](IAsyncOperation<winrt::hstring> info, AsyncStatus status) {
+          if (status == AsyncStatus::Completed) {
+            auto text = Microsoft::Common::Unicode::Utf16ToUtf8(info.GetResults());
+            result.Resolve(React::JSValue{ text });
+          }
+          else {
+            result.Reject("Failure");
+          }
+        });
+        return;
+      }
+      result.Resolve("");
+    }
+  }
+
+  void ClipboardModule::SetString(std::string const& str) noexcept
+  {
+    datatransfer::DataPackage dataPackage{};
+    dataPackage.SetText(Microsoft::Common::Unicode::Utf8ToUtf16(str));
+    datatransfer::Clipboard::SetContent(dataPackage);
+  }
+}
--- windows/Clipboard/Clipboard.h
+++ windows/Clipboard/Clipboard.h
@@ -4,7 +4,6 @@
 #include <functional>
 #include <string>
 #include <NativeModules.h>
-#include <../Common/unicode.h>
 
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.ApplicationModel.DataTransfer.h>
@@ -14,41 +13,14 @@ using namespace winrt::Windows::Foundation;
 namespace datatransfer = winrt::Windows::ApplicationModel::DataTransfer;
 
 namespace NativeClipboard {
-    REACT_MODULE(ClipboardModule, L"RNCClipboard");
-    struct ClipboardModule
-    {
+  REACT_MODULE(ClipboardModule, L"RNCClipboard");
+  struct ClipboardModule
+  {
 
-        REACT_METHOD(GetString, L"getString");
-        void GetString(bool error, React::ReactPromise<React::JSValue>&& result) noexcept
-        {
-            if (error) {
-                result.Reject("Failure");
-            }
-            else {
-                auto dataPackageView = datatransfer::Clipboard::GetContent();
-                if (dataPackageView.Contains(datatransfer::StandardDataFormats::Text())) {
-                    dataPackageView.GetTextAsync().Completed([result, dataPackageView](IAsyncOperation<winrt::hstring> info, AsyncStatus status) {
-                        if (status == AsyncStatus::Completed) {
-                            auto text = ::Microsoft::Common::Unicode::Utf16ToUtf8(info.GetResults());
-                            result.Resolve(React::JSValue{ text });
-                        }
-                        else {
-                            result.Reject("Failure");
-                        }
-                    });
-                    return;
-                }
-                result.Resolve("");
-            }
-        }
+    REACT_METHOD(GetString, L"getString");
+    void GetString(bool error, React::ReactPromise<React::JSValue>&& result) noexcept;
 
-        REACT_METHOD(SetString, L"setString");
-        void SetString(std::string const& str) noexcept
-        {
-            datatransfer::DataPackage dataPackage{};
-            dataPackage.SetText(::Microsoft::Common::Unicode::Utf8ToUtf16(str));
-            datatransfer::Clipboard::SetContent(dataPackage);
-        }
-
-    };
+    REACT_METHOD(SetString, L"setString");
+    void SetString(std::string const& str) noexcept;
+  };
 }
