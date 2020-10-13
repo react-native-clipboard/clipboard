a34b4e0a280b3e281e6c665dd4d9cef2a5bc2faf
Brendon Sled <brendon@tinyroar.com>
2020-10-13 15:35:08 -0600

    Added subset navigator type definition

--- src/Clipboard.web.ts
+++ src/Clipboard.web.ts
@@ -1,3 +1,10 @@
+declare var navigator: {
+  clipboard: {
+    readText(): Promise<string>;
+    writeText(data: string): Promise<void>;
+  };
+};
+
 export const Clipboard = {
   getString(): Promise<string> {
     return navigator.clipboard.readText();
