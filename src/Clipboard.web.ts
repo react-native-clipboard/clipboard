273a488b8d43b3e3a8d3e05991a2dc038613d97c
Brendon Sled <brendon@tinyroar.com>
2021-02-20 13:31:22 -0600

    Back compat support for older browsers

--- src/Clipboard.web.ts
+++ src/Clipboard.web.ts
@@ -5,12 +5,33 @@ declare var navigator: {
   };
 };
 
+declare var document: any;
+
 export const Clipboard = {
   getString(): Promise<string> {
-    return navigator.clipboard.readText();
+    if (navigator && navigator.clipboard){
+      return navigator.clipboard.readText();
+    } else {
+      const el = document.createElement('textarea');
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('paste');
+      const value = el.innerText;
+      document.body.removeChild(el);
+      return Promise.resolve(value);
+    }
   },
 
   setString(content: string) {
-    navigator.clipboard.writeText(content);
+    if (navigator && navigator.clipboard){
+      navigator.clipboard.writeText(content);
+    } else {
+      const el = document.createElement('textarea');
+      el.value = content;
+      document.body.appendChild(el);
+      el.select();
+      document.execCommand('copy');
+      document.body.removeChild(el);
+    }
   },
 };
