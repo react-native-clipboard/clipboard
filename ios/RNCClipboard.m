f089a31e8c5b6dd6ee3507a72b4f38d293531bb3
Atul Madhugiri <atulm@berkeley.edu>
2021-05-11 10:26:28 -0400

    objc: getImage[png/jpg] + hasImage

--- ios/RNCClipboard.m
+++ ios/RNCClipboard.m
@@ -28,6 +28,50 @@ RCT_EXPORT_METHOD(getString:(RCTPromiseResolveBlock)resolve
   resolve((clipboard.string ? : @""));
 }
 
+RCT_EXPORT_METHOD(getImagePNG:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+  NSString *pngPrefix = @"data:image/png;base64,";
+  UIPasteboard *clipboard = [UIPasteboard generalPasteboard];
+  UIImage *clipboardImage = clipboard.image;
+  if (!clipboardImage) {
+    resolve(NULL);
+  } else {
+    NSString *imageDataBase64 = [UIImagePNGRepresentation(clipboardImage) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    NSString *withPrefix = [pngPrefix stringByAppendingString:imageDataBase64];
+    resolve((withPrefix ? : NULL));
+  }
+}
+
+RCT_EXPORT_METHOD(getImageJPG:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+  NSString *jpgPrefix = @"data:image/jpeg;base64,";
+  UIPasteboard *clipboard = [UIPasteboard generalPasteboard];
+  UIImage *clipboardImage = clipboard.image;
+  if (!clipboardImage) {
+    resolve(NULL);
+  } else {
+    NSString *imageDataBase64 = [UIImageJPEGRepresentation(clipboardImage, 1.0) base64EncodedStringWithOptions:NSDataBase64Encoding64CharacterLineLength];
+    NSString *withPrefix = [jpgPrefix stringByAppendingString:imageDataBase64];
+    resolve((withPrefix ? : NULL));
+  }
+}
+
+RCT_EXPORT_METHOD(hasImage:(RCTPromiseResolveBlock)resolve
+                  reject:(__unused RCTPromiseRejectBlock)reject)
+{
+  BOOL imagePresent = YES;
+  UIPasteboard *clipboard = [UIPasteboard generalPasteboard];
+  if (@available(iOS 10, *)) {
+    imagePresent = clipboard.hasImages;
+  } else {
+    UIImage *imageInPasteboard = clipboard.image;
+    imagePresent = imageInPasteboard != nil;
+  }
+  resolve([NSNumber numberWithBool: imagePresent]);
+}
+
 RCT_EXPORT_METHOD(hasString:(RCTPromiseResolveBlock)resolve
                   reject:(__unused RCTPromiseRejectBlock)reject)
 {
