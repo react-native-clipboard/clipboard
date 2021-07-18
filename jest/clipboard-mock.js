1f0f94ba8192277898a05192d9d89555279bc3c7
Jesse Katsumata <jesse.katsumata@gmail.com>
2021-07-18 20:20:38 +0900

    feat: add clipboard mock

---
+++ jest/clipboard-mock.js
@@ -0,0 +1,23 @@
+/**
+ * @format
+ */
+/* eslint-env jest */
+
+const ClipboardMock = {
+  getString: jest.fn().mockResolvedValue('mockString'),
+  getImagePNG: jest.fn(),
+  getImageJPG: jest.fn(),
+  setString: jest.fn(),
+  hasString: jest.fn().mockResolvedValue(true),
+  hasImage: jest.fn().mockResolvedValue(true),
+  hasURL: jest.fn().mockResolvedValue(true),
+  addListener: jest.fn(),
+  removeAllListeners: jest.fn(),
+};
+
+const RNCClipboardMock = {};
+
+RNCClipboardMock.Clipboard = ClipboardMock;
+RNCClipboardMock.useClipboard = jest.fn(() => ['mockString', jest.fn()]);
+
+module.exports = RNCClipboardMock;
