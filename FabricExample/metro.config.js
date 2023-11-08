4031de5d13648cf11204873b470064b371d51cbb
Wojciech Lewicki <wojciech.lewicki@swmansion.com>
2023-11-08 13:35:18 +0100

    feat: update FabricExample to 0.72

--- FabricExample/metro.config.js
+++ FabricExample/metro.config.js
@@ -1,4 +1,11 @@
-/* eslint-disable import/no-commonjs */
+const {getDefaultConfig, mergeConfig} = require('@react-native/metro-config');
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
 
 const path = require('path');
 const exclusionList = require('metro-config/src/defaults/exclusionList');
@@ -11,32 +18,34 @@ const modules = [
   ...Object.keys(pack.peerDependencies),
 ];
 
-module.exports = {
-  projectRoot: __dirname,
-  watchFolders: [root],
-
-  // We need to make sure that only one version is loaded for peerDependencies
-  // So we exclude them at the root, and alias them to the versions in example's node_modules
-  resolver: {
-    blacklistRE: exclusionList(
-      modules.map(
-        (m) =>
-          new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+const config = {
+    projectRoot: __dirname,
+    watchFolders: [root],
+  
+    // We need to make sure that only one version is loaded for peerDependencies
+    // So we exclude them at the root, and alias them to the versions in example's node_modules
+    resolver: {
+      blacklistRE: exclusionList(
+        modules.map(
+          (m) =>
+            new RegExp(`^${escape(path.join(root, 'node_modules', m))}\\/.*$`),
+        ),
       ),
-    ),
+  
+      extraNodeModules: modules.reduce((acc, name) => {
+        acc[name] = path.join(__dirname, 'node_modules', name);
+        return acc;
+      }, {}),
+    },
+  
+    transformer: {
+      getTransformOptions: async () => ({
+        transform: {
+          experimentalImportSupport: false,
+          inlineRequires: true,
+        },
+      }),
+    },
+  };
 
-    extraNodeModules: modules.reduce((acc, name) => {
-      acc[name] = path.join(__dirname, 'node_modules', name);
-      return acc;
-    }, {}),
-  },
-
-  transformer: {
-    getTransformOptions: async () => ({
-      transform: {
-        experimentalImportSupport: false,
-        inlineRequires: true,
-      },
-    }),
-  },
-};
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);
