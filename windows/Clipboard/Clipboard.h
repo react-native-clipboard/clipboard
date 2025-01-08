b18b6267110aabc1218f84685c7d2e5d723b5253
Yajur Grover <157416350+Yajur-Grover@users.noreply.github.com>
2025-01-07 19:21:56 -0800

    feat: add Windows New Arch Support (#278)

--- windows/Clipboard/Clipboard.h
+++ windows/Clipboard/Clipboard.h
@@ -1,9 +1,16 @@
 #pragma once
+
 #include "pch.h"
+#include "resource.h"
+
+#if __has_include("codegen/NativeClipboardDataTypes.g.h")
+  #include "codegen/NativeClipboardDataTypes.g.h"
+#endif
+#include "codegen/NativeClipboardModuleSpec.g.h"
 
 #include <functional>
 #include <string>
-#include <NativeModules.h>
+#include "NativeModules.h"
 
 #include <winrt/Windows.Foundation.h>
 #include <winrt/Windows.ApplicationModel.DataTransfer.h>
@@ -12,16 +19,17 @@ using namespace winrt::Microsoft::ReactNative;
 using namespace winrt::Windows::Foundation;
 namespace datatransfer = winrt::Windows::ApplicationModel::DataTransfer;
 
-namespace NativeClipboard {
+namespace winrt::Clipboard
+{
+
   REACT_MODULE(ClipboardModule, L"RNCClipboard");
   struct ClipboardModule
   {
     REACT_INIT(Initialize);
-    void Initialize(const winrt::Microsoft::ReactNative::ReactContext& reactContext) noexcept
-    {
-      _context = reactContext;
+    void Initialize(const winrt::Microsoft::ReactNative::ReactContext& reactContext) noexcept {
+      m_context = reactContext;
     }
-
+    
     REACT_METHOD(GetString, L"getString");
     void GetString(React::ReactPromise<std::string>&& result) noexcept;
 
@@ -36,6 +44,7 @@ namespace NativeClipboard {
 
   private:
     int _listenerCount = 0;
-    ReactContext _context;
+    winrt::Microsoft::ReactNative::ReactContext m_context;
   };
-}
+
+} // namespace winrt::Clipboard
